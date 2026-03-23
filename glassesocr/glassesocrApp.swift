5996fd31277f0483da2499e1b6044ff53391372a
campb247 <campb247@csusm.edu>
2026-03-23 09:21:02 -0700

    Initial Commit

---
+++ glassesocr/glassesocrApp.swift
@@ -0,0 +1,17 @@
+//
+//  glassesocrApp.swift
+//  glassesocr
+//
+//  Created by Kaden Campbell on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct glassesocrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
